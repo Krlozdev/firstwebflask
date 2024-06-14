cbf05be7e8f2511cb7881131c644aa2434c5baf3
krloz <krlozexile@gmail.com>
2024-06-13 20:56:53 -0500

    ejercicios clase programacion

---
+++ semana_9b.py
@@ -0,0 +1,31 @@
+# i = 1
+# while i <= 50:
+#     i = 3 * i + 1
+#     print("fin del programa")
+
+# num = int(input("Ingrese un numero positivo: "))
+# while num < 0:
+#     print("error, ingrese un numero positivo")
+#     num = int(input("Ingrese un numero positivo: "))
+# print("ahora si continuamos")
+
+# for i in range(20):
+#
+#     print(i)
+#
+#
+# for i in reversed(range(20)):
+#       print(i)
+
+# for letra in "python":
+#       print(f"dame una letra: {letra}")
+#
+# print("!PYTHON¡")
+#
+# print("comienzo")
+# for i in [0,1,2,3,4,5,6,7,8,9]:
+#       print(i)
+
+
+for i in [3,4,5]:
+    print("ahora i vale ", i, "y su cuadrado", i**2)
