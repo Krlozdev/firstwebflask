1e3173377e6aac6e2921935086cca6bee8a41f7e
krloz <krlozexile@gmail.com>
2024-06-17 22:57:20 -0500

    ejercicios clase programacion

---
+++ simulacroEC3.py
@@ -0,0 +1,69 @@
+# # ejercio 1
+#
+# contador = 1
+# while contador <= 10:
+#     print(contador)
+#     contador += 1
+
+# ejercio 2
+
+# num = int(input("Ingrese un numero positivo: "))
+#
+# suma = 1
+# contador = 1
+#
+# while contador <= num:
+#     suma += contador
+#     contador += 1
+# print("la suma de los numeros del 1 al {} es: ".format(contador), suma)
+
+# ejercicio 3
+#
+# num = int(input("Ingrese un numero entero: "))
+#
+# multiplicacion = 1
+#
+# while multiplicacion <=10:
+#     producto = multiplicacion * num
+#     print("la multiplicacion de {} por {} es: {}".format(num,multiplicacion,producto))
+#     multiplicacion += 1
+#
+
+#ejercicio 4
+
+password_sql = "nevor3122"
+
+while password_sql == "nevor3122":
+    contraseña = input("ingrese su contraseña: ")
+    if contraseña == password_sql:
+        print("Bienvenido al Sistema")
+        break
+    else:
+        print("Contraseña incorrecta, intente nuevamente")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
